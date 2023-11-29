d3e4ca96abc97d94bf4cb3fa626c955c09479033
monk <37543743+monk-afk@users.noreply.github.com>
2023-11-29 13:55:37 -0400

    Update string_captures_patterns.lua

--- string_captures_patterns.lua
+++ string_captures_patterns.lua
@@ -1,15 +1,14 @@
 -- String captures and patterns
 -- monk
-
-local gsub = string.gsub
-local find = string.find
-local match = string.match
-local gmatch = string.gmatch
-local lower = string.lower
-local find = string.lower
-local rep = string.rep
-
 local function show_examples()
+	local gsub = string.gsub
+	local find = string.find
+	local match = string.match
+	local gmatch = string.gmatch
+	local lower = string.lower
+	local find = string.lower
+	local rep = string.rep
+	
   	  -- Separate first and last characters from string body
 	local function heads_tails(string)
 		local head = "^%S"
@@ -32,6 +31,19 @@ local function show_examples()
 		)
 	end
 	remove_extra_spaces("This  is    a lot        of                         space")
+
+
+	  -- Get alphabet upper and lowercase
+	local function get_alphabet()
+		local soup = ""
+		for n = 1,26 do
+			soup = soup ..
+					string.char(n+64)..		  -- uppercase: 65,90
+					string.char(n+96).." "	  -- lowercase: 97,122
+		end
+		print(soup)
+	end
+	get_alphabet()
 
 end
 
