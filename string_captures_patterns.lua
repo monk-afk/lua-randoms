08c944539f5d4dd5501b5d4febb99717503c0b91
monk <37543743+monk-afk@users.noreply.github.com>
2023-11-30 17:25:19 -0400

    Update string_captures_patterns.lua

--- string_captures_patterns.lua
+++ string_captures_patterns.lua
@@ -1,72 +1,90 @@
 -- String captures and patterns
--- monk
-local gsub = string.gsub
-local find = string.find
-local match = string.match
-local gmatch = string.gmatch
-local lower = string.lower
-local find = string.lower
-local rep = string.rep
-local byte = string.byte
-local char = string.char
+	-- monk
 
-	  -- Split string by words then by character
-local function split_string(string, s)
-print(string)
-	for word in string:gmatch("%S+") do
-		for character in word:gmatch("%S") do
-			s = s .. character .. " "
+	local gsub = string.gsub
+	local find = string.find
+	local match = string.match
+	local gmatch = string.gmatch
+	local lower = string.lower
+	local find = string.lower
+	local rep = string.rep
+	local byte = string.byte
+	local char = string.char
+
+-- Split string by words then by character
+	local function split_string(string, s)
+		for word in gmatch(string, "%S+") do
+			for character in gmatch(word, "%S") do
+				s = s .. character .. " "
+			end
+		s = s .. " "
 		end
-	s = s .. " "
+	print(s)
 	end
-print(s)
-end
-split_string("Make like a banana and split!","")
+	split_string("Make like a banana and split!","")
 
 
-	-- Separate first and last characters from string body
-local function heads_tails(string)
-	local head = "^%S"
-	local body = "%S(.+)([%S%s])"
-	local tail = "%S$"
-		print(string)
+-- Separate first and last characters from string body
+	local function heads_tails(string)
+		local head = "^%S"
+		local body = "%S(.+)([%S%s])"
+		local tail = "%S$"
+			print(
+				match(string, head) .." | "..
+				match(string, body) .." | "..
+				match(string, tail)
+			)
+	end
+	heads_tails("heads or tails")
+
+
+-- Remove extra spaces
+	local function remove_extra_spaces(string)
 		print(
-			string:match(head) .." | "..
-					string:match(body) .." | "..
-							string:match(tail)
-		)
-end
-heads_tails("heads or tails")
-
-	-- Remove extra spaces
-local function remove_extra_spaces(string)
-	print(string)
-	print(
-		string:gsub("%s+", " ")
-	)
-end
-remove_extra_spaces("This  is    a lot        of                         space")
-
-
-	-- Get alphabet upper and lowercase
-local function get_alphabet(soup)
-	for n = 1,26 do
-		soup = soup ..
-				soup.char(n+64)..		  -- uppercase: 65,90
-				soup.char(n+96).." "	  -- lowercase: 97,122
-	end									  -- Standard: 33, 126
-	print(soup)
-end
-get_alphabet("")
-
-
-  -- Get byte integer of non-latin characters
-local function character_bytes(string)
-	for character in string:gmatch("[^%sA-z]") do
-		local charbyte = character:byte()
-		print(
-			char(charbyte) .. " = ".. charbyte
+			gsub(string, "%s+", " ")
 		)
 	end
-end
-character_bytes("caractères latinisés français")
+	remove_extra_spaces("This  is    a lot        of                         space")
+
+
+-- Get alphabet upper and lowercase
+	local function get_alphabet(soup)
+		for n = 1,26 do
+			soup = soup ..
+					soup.char(n+64)..		  -- uppercase: 65,90
+					soup.char(n+96).." "	  -- lowercase: 97,122
+		end									  -- Standard: 33, 126
+		print(soup)
+	end
+	get_alphabet("")
+
+
+-- Get byte integer of non-latin characters
+	local function character_bytes(string)
+		for character in gmatch(string, "[^%sA-z]") do
+			local charbyte = byte(character)
+			print(
+				char(charbyte) .. " = ".. charbyte
+			)
+		end
+	end
+	character_bytes("caractères latinisés français")
+
+
+-- Repeat a pattern
+	local function repeat_pattern(string,s)
+		for character in gmatch(string, "%S") do
+			-- print(
+				s = s .. match(string, character):rep(#string)
+			-- )
+		end
+	print(s)
+	end
+	repeat_pattern("Stutter","")
+
+
+-- Remove repeating characters
+	local function no_repeat(string)
+			print(gsub(string, "(%S)%S+", "%1"))
+	end
+	no_repeat("1 22 333 4444 55555 666666 7777777 888888888 999999999")
