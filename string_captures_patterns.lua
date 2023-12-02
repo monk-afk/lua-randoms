2e3432f764c6cff2dc322ffcc874f3f2596669f8
monk <37543743+monk-afk@users.noreply.github.com>
2023-12-01 22:43:55 -0400

    Update string_captures_patterns.lua

--- string_captures_patterns.lua
+++ string_captures_patterns.lua
@@ -85,6 +85,6 @@
 
 -- Remove repeating characters
 	local function no_repeat(string)
-			print(gsub(string, "(%S)%S+", "%1"))
+			print(gsub(string, "([%S])%S+%1", "%1")) -- only doubles are left, eg: `hello`
 	end
 	no_repeat("1 22 333 4444 55555 666666 7777777 888888888 999999999")
