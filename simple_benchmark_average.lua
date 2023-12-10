dea2d696076faf7614e5f19c06e90d2061562568
monk <37543743+monk-afk@users.noreply.github.com>
2023-12-10 03:50:40 -0400

    fixed port

--- simple_benchmark_average.lua
+++ simple_benchmark_average.lua
@@ -1,19 +1,24 @@
   -- Average CPU clock time for simple benchmarking
-  --[[ monk ]]
-  -- 
+  --[[ monk ]]--
+  -- 0.02
 local avg = 0
 local ct = 0
 local frq = 0
-function benchmark()
-	while true do  -- infinite loop
+
+--[[ function benchmark() ]]--
+
+	while true do
 		local osc = os.clock()
 		frq = frq + 1
 
---[[	insert function here	]]
+--[[	insert function here	]]--
 
 		local isc = os.clock()
 		ct = ct + (isc - osc)
 		avg = (ct/frq)
 	end
-end
-print(tonumber(string.format("%.6f", benchmark())))
+	print(
+		tonumber(
+			string.format("%.6f", avg)))
+
+--[[ end ]]--
