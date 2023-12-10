e29603c0d036643a8e9d977cd014c30e40f482b7
monk <37543743+monk-afk@users.noreply.github.com>
2023-12-10 00:08:14 -0400

    Update pattern_test.lua

--- pattern_test.lua
+++ pattern_test.lua
@@ -1,5 +1,54 @@
--- string replacemets
--- 0.01
+-- debug filtering test
+local lower = string.lower
+local gsub = string.gsub
+
+local strings = { --> todo, get messages from stream output
+	[1] = "2023-12-08 03:43:24: ACTION[Server]: gongong dies at (0,-1,0). Inventory dropped",
+	[2] = "2023-11-25 20:23:19: ACTION[Server]: RealSuperd places node default:silver_sandstone_block at (-7,4,1)",
+	[3] = "2023-11-23 14:10:16: ACTION[Server]: ElinetUwu [10.0.0.1] joins game. List of players: ",
+	[4] = "2023-11-23 14:11:37: ACTION[Server]: Quintin digs default:cobble at (-1,1,1)",
+	[5] = "2023-11-25 20:23:11: ACTION[Server]: player Addicat (id=63352, hp=20) punched player Zeratul (id=41118, hp=15), damage=0 (handled by Lua)",
+}
+
+local sweep = function(string)
+		return gsub(string, "^([%d-%s:]+):%s%S+%s(%S+)%s([%[%d.%]]?%s?%S+)%s(%S+)%s(%S+)",
+			function(date,name,action,dnode,node)
+				print(date .. " | ".. name.." | ".. action.." | ".. dnode.." | ".. node)
+				return
+			end)
+end
+
+for i = 1,#strings do
+	local message = strings[i]
+	print("Orignal: "..message.."\nOutput: "..sweep(message).."\n")
+end
+
+
+
+		-- 2023-12-08 03:43:24 | gongong | dies | at | (0,-1,0).
+		-- Orignal: 2023-12-08 03:43:24: ACTION[Server]: gongong dies at (0,-1,0). Inventory dropped
+		-- Output: 2023-12-08 03:43:24: ACTION[Server]: gongong dies at (0,-1,0). Inventory dropped
+		
+		-- 2023-11-25 20:23:19 | RealSuperd | places | node | default:silver_sandstone_block
+		-- Orignal: 2023-11-25 20:23:19: ACTION[Server]: RealSuperd places node default:silver_sandstone_block at (-7,4,1)
+		-- Output: 2023-11-25 20:23:19: ACTION[Server]: RealSuperd places node default:silver_sandstone_block at (-7,4,1)
+		
+		-- 2023-11-23 14:10:16 | ElinetUwu | [10.0.0.1] | joins | game.
+		-- Orignal: 2023-11-23 14:10:16: ACTION[Server]: ElinetUwu [10.0.0.1] joins game. List of players: 
+		-- Output: 2023-11-23 14:10:16: ACTION[Server]: ElinetUwu [10.0.0.1] joins game. List of players: 
+		
+		-- 2023-11-23 14:11:37 | Quintin | digs | default:cobble | at
+		-- Orignal: 2023-11-23 14:11:37: ACTION[Server]: Quintin digs default:cobble at (-1,1,1)
+		-- Output: 2023-11-23 14:11:37: ACTION[Server]: Quintin digs default:cobble at (-1,1,1)
+		
+		-- 2023-11-25 20:23:11 | player | Addicat | (id=63352, | hp=20)
+		-- Orignal: 2023-11-25 20:23:11: ACTION[Server]: player Addicat (id=63352, hp=20) punched player Zeratul (id=41118, hp=15), damage=0 (handled by Lua)
+		-- Output: 2023-11-25 20:23:11: ACTION[Server]: player Addicat (id=63352, hp=20) punched player Zeratul (id=41118, hp=15), damage=0 (handled by Lua)
+
+
+
+
+-- irregular patterns test
 local lower = string.lower
 local gsub = string.gsub
 
@@ -33,20 +82,20 @@ end
 
 
 
-	-- Orignal: ur a BIG mother f, l, o, w, e, r,
-	-- Output: ura big motherflower
+-- 	-- Orignal: ur a BIG mother f, l, o, w, e, r,
+-- 	-- Output: ura big motherflower
 
-	-- Orignal: L U C K Y O U MONK
-	-- Output: luckyou monk
+-- 	-- Orignal: L U C K Y O U MONK
+-- 	-- Output: luckyou monk
 
-	-- Orignal: MiXeD | T  e   X     t (ç, ã, é, ñ, ß, Ł, £ ¥) %_$-#=  [~1+2*3%4-5/6`]
-	-- Output: mixedtext123456
+-- 	-- Orignal: MiXeD | T  e   X     t (ç, ã, é, ñ, ß, Ł, £ ¥) %_$-#=  [~1+2*3%4-5/6`]
+-- 	-- Output: mixedtext123456
 
-	-- Orignal: 1 22 333 4444 55555 666666 7777777 888888888 999999999
-	-- Output: 1 2 3 4 5 6 7 8 9
+-- 	-- Orignal: 1 22 333 4444 55555 666666 7777777 888888888 999999999
+-- 	-- Output: 1 2 3 4 5 6 7 8 9
 
-	-- Orignal: ffffffuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuunny
-	-- Output: funy
+-- 	-- Orignal: ffffffuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuunny
+-- 	-- Output: funy
 
-	-- Orignal: mid-text g a p s in words
-	-- Output: mid textgaps in words
+-- 	-- Orignal: mid-text g a p s in words
+-- 	-- Output: mid textgaps in words
